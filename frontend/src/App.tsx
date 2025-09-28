1e4529e21aa5fd53412997ec854bc95f4ad97ee9
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-09-28 07:10:48 +0530

    created the interview form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 // import { Button } from "@/components/ui/button"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import CreateAccount from "./pages/createAccount";
-// import StartInterview from "./pages/startInterview";
+import StartInterview from "./pages/startInterview";
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/account" element={<CreateAccount />} />
-        {/* <Route path="/interview" element={<StartInterview />} /> */}
+        <Route path="/interview" element={<StartInterview />} />
       </Routes>
     </BrowserRouter>
   )
