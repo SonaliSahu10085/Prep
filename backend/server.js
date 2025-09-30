891f3162131da9197ee4a5ee50baa1133dd97be3
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-09-30 09:47:21 +0530

    feat: created the notFound and errorHandling middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const studentRouter = require("./routes/studentRoute");
+const { notFound } = require("./middlewares/notFound");
+const { errorHandling } = require("./middlewares/errorHandling");
 
 connectDB();
 const app = express();
@@ -11,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/student", studentRouter);
+
+app.use(notFound);
+app.use(errorHandling);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
