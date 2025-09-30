81c29405380c16c5082802902df4614054493d5c
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-09-30 10:18:44 +0530

    feat : setup up the mrogan middleware for log all the info related to the apis

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const studentRouter = require("./routes/studentRoute");
 const { notFound } = require("./middlewares/notFound");
 const { errorHandling } = require("./middlewares/errorHandling");
+const morgan = require("morgan");
 
 connectDB();
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/student", studentRouter);
 
