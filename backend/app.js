257c0763493793fcb53c3719c47eb68b3f6939ef
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-10-06 09:25:36 +0530

    feat : created the roadmap routes and all setup is done for the roadmap module

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const studentRouter = require("./routes/studentRoutes");
+const roadmapRouter = require("./routes/roadmapRoutes");
 const { notFound } = require("./middlewares/notFound");
 const { errorHandling } = require("./middlewares/errorHandling");
 const morgan = require("morgan");
 
+// database connection
 connectDB();
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// routes
 app.use("/student", studentRouter);
+app.use("/roadmap", roadmapRouter);
 
+// error handling middleware
 app.use(notFound);
 app.use(errorHandling);
 
