d13b71af49e20a1b008576ee9be91360a2b49db3
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-10-06 09:30:49 +0530

    fix : the error of getAllRoadmaps and code is done of the getAllRoadmaps

--- backend/controllers/roadmapController.js
+++ backend/controllers/roadmapController.js
@@ -1,13 +1,21 @@
 const Roadmap = require("../models/roadmapModel");
 
-const getAllRoadmap = async (req, res) => {
+// Get all roadmaps
+const getAllRoadmaps = async (req, res) => {
     try {
-        
+        const roadmaps = await Roadmap.find({});
+
+        if (!roadmaps) {
+            return res.status(404).json({ message: "No roadmaps found" });
+        }
+
+        res.status(200).json({ message: "Roadmaps fetched successfully", roadmaps });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
 
+// Get roadmap by year
 const getRoadmapByYear = async (req, res) => {
     try {
         
@@ -16,6 +24,7 @@ const getRoadmapByYear = async (req, res) => {
     }
 };
 
+// Create roadmap
 const createRoadmap = async (req, res) => {
     try {
         
@@ -24,6 +33,7 @@ const createRoadmap = async (req, res) => {
     }
 };
 
+// Update roadmap
 const updateRoadmap = async (req, res) => {
     try {
         
@@ -32,6 +42,7 @@ const updateRoadmap = async (req, res) => {
     }
 };
 
+// Delete roadmap
 const deleteRoadmap = async (req, res) => {
     try {
         
@@ -41,7 +52,7 @@ const deleteRoadmap = async (req, res) => {
 };
 
 module.exports = {
-    getAllRoadmap,
+    getAllRoadmaps,
     getRoadmapByYear,
     createRoadmap,
     updateRoadmap,
