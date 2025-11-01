1cf65e891565836a11aaae853668e9b6fb416556
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-11-01 15:09:28 +0530

    All testing is done and just fix one routing order issue of the resource module

--- backend/routes/resourceRoutes.js
+++ backend/routes/resourceRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const resourceController = require("../controllers/resourceController");
-const { authVerify} = require("../middlewares/authMiddleware");
+const { authVerify } = require("../middlewares/authMiddleware");
 
 // Resource routes
-router.get("/:topic", authVerify, resourceController.getResourceByTopic);
 router.get("/", authVerify, resourceController.getAllResources);
+router.get("/:topic", authVerify, resourceController.getResourceByTopic);
 
 // Only for admin
-router.post("/", resourceController.createResource); 
+router.post("/", resourceController.createResource);
 router.patch("/:id", resourceController.updateResource);
 router.delete("/:id", resourceController.deleteResource);
 
