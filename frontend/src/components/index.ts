c54311e100367a9b6131cb05f55724a12adb6af9
Sonali Sahu <sonalisahu0064@gmail.com>
2025-11-09 21:02:50 +0530

    Feat: Completed the PrepPartner Component Section, Only a little bit organization left which I'll do after completing the home page

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,11 +2,13 @@ import Form from "./Form";
 import Navbar from "./Navbar";
 import Body from "./Body";
 import HeroSection from "./HeroSection";
+import PrepPartner from "./PrepPartner";
 
 
 export {
     Form,
     Navbar,
     Body,
-    HeroSection
+    HeroSection,
+    PrepPartner
 }
--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,15 +1,17 @@
 const { VITE_CLOUDINARY_BASE_URL: CLOUDINARY_BASE_URL } = import.meta.env;
 
-
-
 export const images = {
   default: {
     heroSection: `${CLOUDINARY_BASE_URL}/v1761397617/hero_section_ddbswz.png`,
     logo: `${CLOUDINARY_BASE_URL}/v1761397617/logo_ya3duz.png`,
+    ajay: `${CLOUDINARY_BASE_URL}/v1762683800/ajay_jaawpk.png`,
+    sonali: `${CLOUDINARY_BASE_URL}/v1762683800/sonali_vrvlk4.png`,
   },
   light: {
     bgImage: `${CLOUDINARY_BASE_URL}/v1761397617/BG_Light_qgtgt5.png`,
     adminLogin: `${CLOUDINARY_BASE_URL}/v1761397617/admin_portal_light_tvpycn.png`,
+    ourMission: `${CLOUDINARY_BASE_URL}/v1762683800/OurMission_llzxml.png`,
+    weServe: `${CLOUDINARY_BASE_URL}/v1762683800/people_group_dxwpq1.png`
   },
   dark: {
     bgImage: `${CLOUDINARY_BASE_URL}/v1761562643/bgDark_fb95gi.png`,
